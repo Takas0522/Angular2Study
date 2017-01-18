3c099e418e38ba96f6746d7fd88821ddfc48ed96
t.Ohkawa <t_o_19880522@outlook.com>
2017-01-18 16:20:32 +0900

    <Select>[ngModel] Value changed "string" type!

--- AngularsFunctionStudy/app/area/ngFor/ngFor.Component.ts
+++ AngularsFunctionStudy/app/area/ngFor/ngFor.Component.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 @Component({
     template: `
     <h2>ngFor</h2>
@@ -10,10 +10,31 @@ import {Component} from "@angular/core";
     <div *ngFor="let item of showArray">
         {{item.code}} / {{item.name}}
     </div>
+    <select [(ngModel)]="selectedValue"
+            (ngModelChange)="onSelectedValueChange()">
+        <option *ngFor="let item of selectArray"
+                [value]="item.code">
+            {{item.name}}
+        </option>
+    </select>
     `
 })
-export class NgForComponent{
+export class NgForComponent implements OnInit{
     private showArray: SampleArray[] = [{code: 1, name:"TKS0522"}, {code: 2, name: "HogeHoge"},{code: 3, name:"FugaFuga"}];
+    private selectArray: SampleArray[] = [{code: 10, name:"AAA"}, {code: 20, name: "BBB"},{code: 30, name:"CCC"}];
+    private selectedValue: number;
+    ngOnInit(){
+        this.selectedValue = 10;
+        /*JSON Value nothing "double quate"*/
+        let jsonData = JSON.stringify(this.selectedValue);
+        console.log(jsonData);
+    }
+    onSelectedValueChange(){
+        let jsonData = JSON.stringify(this.selectedValue);
+        /*add "double quate"!*/
+        console.log(typeof this.selectedValue);
+        console.log(jsonData);
+    }
 }
 class SampleArray {
     code: number;
