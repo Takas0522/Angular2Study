64c33c904e875b6b9e4f7df3713c6dd01347e256
TKS0522 <t_o_19880522@outlook.com>
2017-06-13 22:24:57 +0900

    ひとまず使い方はOK

--- UseAngularMaterial/src/app/app.module.ts
+++ UseAngularMaterial/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdInputModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MdInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
