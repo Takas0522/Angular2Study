284b7358767bf0f6bfb884a3a47d121a91212feb
TKS0522 <t_o_19880522@outlook.com>
2017-07-13 07:03:02 +0900

    Update

--- StudyProgressiveWebApps/e2e/app.e2e-spec.ts
+++ StudyProgressiveWebApps/e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('study-progressive-web-apps App', () => {
     page = new StudyProgressiveWebAppsPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
   });
 });
